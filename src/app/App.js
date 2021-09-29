840344c697ae80b398762e271564793a9862b54f
Balaji Davangave <balajidavangavem@gmail.com>
2021-09-29 14:57:23 +0530

    Final push

---
+++ src/app/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Map from "./Map";
+
+import { NavBar } from "../components";
+import { MoviesList, MoviesInsert, MoviesUpdate } from "../pages";
+
+import "./App.css";
+
+import MaterialTable from "material-table";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  const [data, setData] = useState([]);
+  const columns = [
+    // { title: "ID", field: "id" },
+    //{ title: "success", field: "success" },
+    // { title: "City", field: "City" },
+    { title: "Email", field: "email" },
+    // { title: "Phone", field: "phone" },
+    // { title: "Web Link", field: "website" },
+  ];
+  useEffect(() => {
+    fetch("http://localhost:3000/api/movies/")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setData(resp);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 align="center">React-App</h1>
+      <h4 align="center">Material Table</h4>
+      <MaterialTable title="Employee Data" data={data} columns={columns} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { NavBar } from "../components";
+import MaterialTable from "material-table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Link } from "@material-ui/core";
+
+function App() {
+  const [data, setData] = useState([]);
+  const columns = [
+    {
+      title: "ID",
+      field: "_id",
+      render: (rawData) => (
+        <Link
+          href={"http://localhost:3000/api/movie/1000?random=${rawData._id}"}
+        >
+          {rawData._id}
+        </Link>
+      ),
+    },
+    { title: "Name", field: "Name" },
+    { title: "City", field: "City" },
+
+    { title: "Year founded", field: "Year_founded" },
+    { title: "Courses", field: "Courses" },
+    { title: "No of students", field: "No_of_students" },
+    { title: "State", field: "State" },
+    { title: "Country", field: "Country" },
+  ];
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/movies/")
+      .then((resp) => resp.json())
+      // .then((resp) => console.log(resp.data))
+      .then((resp) => {
+        setData(resp.data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 align="center">College Website</h1>
+      <h6 align="center">Assignment created by Davangave Balaji</h6>
+      <MaterialTable
+        title="College List"
+        data={data}
+        columns={columns}
+        // actions={[
+        //   {
+        //     icon: "save",
+        //     tooltip: "Save User",
+        //     onClick: (
+        //       <Link to={"http://localhost:3000/api/movie/${data._id}"}>
+        //         Edit
+        //       </Link>
+        //     ),
+        //   },
+        // ]}
+      />
+    </div>
+  );
+}
+
+export default App;
